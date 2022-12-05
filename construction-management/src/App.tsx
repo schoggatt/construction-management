645d91d38ebf0c8f5ed1ee179b574c26dea3d638
Samuel.Hoggatt <Samuel.Hoggatt@kiewit.com>
2022-12-05 11:41:15 -0600

    Create project list.

--- construction-management/src/App.tsx
+++ construction-management/src/App.tsx
@@ -1,34 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.scss";
+import { Project } from "./models/class/Project";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const projects: Project[] = [
+    new Project(1, "Project: CLASSIFIED", "xXxXC-aAdQ12"),
+    new Project(2, "Project: GIGACHAD", "Chadmin"),
+    new Project(3, "Project: TERRENCE", "Terrel Quigglesbottom"),
+  ];
+
+  function renderProject(project: Project) {
+    return (
+      <li key={project.id}>
+        <h2>{project.name}</h2>
+        <p>{project.description}</p>
+      </li>
+    );
+  }
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Projects</h1>
+      <ul>{projects.map((project) => renderProject(project))}</ul>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ construction-management/src/views/Home/Home.tsx
@@ -0,0 +1,8 @@
+export interface HomeProps {}
+
+export const Home = (props: HomeProps) => {
+	
+	return (
+		<div></div>
+	);
+};
